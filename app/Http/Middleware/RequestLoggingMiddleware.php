86ed00956039fcb3bbdff5a0c4a563805c1a9390
Mircea CALIN <miorita76@gmail.com>
2021-09-21 17:31:14 +0300

    Implement RequestLoggingMiddleware.php without logic

---
+++ app/Http/Middleware/RequestLoggingMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+
+
+class RequestLoggingMiddleware
+{
+    public function handle (Request $request, \Closure $next)
+    {
+        \Log::info('test');
+        
+        return $next($request);
+    }
+}
